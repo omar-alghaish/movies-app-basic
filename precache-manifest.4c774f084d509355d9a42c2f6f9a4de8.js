0f5a0a0203b12120500f0005853ff089c635da15
omar-alghaish <omaralghish@gmail.com>
2022-10-31 21:39:02 +0200

    Updates

---
+++ precache-manifest.4c774f084d509355d9a42c2f6f9a4de8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "557931f60fc19a2577e4",
+    "url": "/movies-app-basic/static/css/main.229f21bc.chunk.css"
+  },
+  {
+    "revision": "557931f60fc19a2577e4",
+    "url": "/movies-app-basic/static/js/main.557931f6.chunk.js"
+  },
+  {
+    "revision": "580b3025cf069a163037",
+    "url": "/movies-app-basic/static/css/1.8f3c32a6.chunk.css"
+  },
+  {
+    "revision": "580b3025cf069a163037",
+    "url": "/movies-app-basic/static/js/1.580b3025.chunk.js"
+  },
+  {
+    "revision": "81d2dea13983b26bdbc5",
+    "url": "/movies-app-basic/static/js/runtime~main.81d2dea1.js"
+  },
+  {
+    "revision": "c87df5975b41e23f2ef3c0382426cadb",
+    "url": "/movies-app-basic/index.html"
+  }
+];
